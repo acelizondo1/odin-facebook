10fa38b2b57b5031fab204d0d83968afbbde8ed6
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-09 13:16:24 -0500

    Adding name to devise User Model with validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject {
+    described_class.new(name: 'Test User',
+                        email: 'test@test.com',
+                        password: '123456')
+  }
+
   it "is valid with valid attributes" do
-    user = User.new(email: 'test@test.com', password: '123456')
-    expect(user).to be_valid
+    expect(subject).to be_valid
   end
 
   it "is not valid without an email" do 
-    user = User.new(email: nil, password: '123456')
-    expect(user).to_not be_valid
+    subject.email = nil
+    expect(subject).to_not be_valid
   end
 
   it "is not valid without a password" do 
-    user = User.new(email: 'test@test.com', password: nil)
-    expect(user).to_not be_valid
+    subject.password = nil
+    expect(subject).to_not be_valid
   end
-
   
 end
