730aeadae03af17cc931d460b3f7549e8b2a1967
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-13 13:47:21 -0500

    Adding Validation to prevent friend requestingexisting friends and friend requesting yourself

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,8 @@
 class Friendship < ApplicationRecord
     after_create :create_inverse_relationship
     after_destroy :destroy_inverse_relationship
+
+    validate :not_self
   
     belongs_to :user
     belongs_to :friend, class_name: 'User'
@@ -23,5 +25,9 @@ class Friendship < ApplicationRecord
     def destroy_inverse_relationship
         friendship = friend.friendships.find_by(friend: user)
         friendship.destroy if friendship
+    end
+
+    def not_self
+        errors.add(:friend, "can't be equal to user") if user == friend
     end
 end
