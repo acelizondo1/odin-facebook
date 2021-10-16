09d69b00ecf1ea5b887bd0293716915714489380
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-15 20:23:04 -0500

    Adding like to posts

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
     belongs_to :user
     belongs_to :post
+
+    scope :locate_like, ->(user_id, post_id){where("user_id = ? AND post_id = ?", user_id, post_id)}
 end
