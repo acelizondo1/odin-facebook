9ea32449933ca4e9fb6d6f1531a74d394a56deb7
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-27 22:28:59 -0500

    Displaying random users on suggest friends section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
     before_action :suggested_users
 
     def suggested_users
-        @suggest_users = User.left_outer_joins(:friendships).where('NOT users.id = ? AND (NOT friendships.friend_id = ? OR friendships.friend_id IS NULL)', current_user.id, current_user.id).limit(5)
+        @suggest_users = User.left_outer_joins(:friendships).where('NOT users.id = ? AND (NOT friendships.friend_id = ? OR friendships.friend_id IS NULL)', current_user.id, current_user.id).order('RANDOM()').limit(5)
     end
 end
