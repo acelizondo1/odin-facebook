5e9d54e2148e3c1028c0285b321de68a2690859a
Aaron Elizondo <acelizondo11@gmail.com>
2021-10-31 18:40:26 -0500

    Adding post photo display if one is present

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -55,7 +55,7 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permit(:body)
+        params.require(:post).permit(:body, :photo)
     end
 
     def set_post
