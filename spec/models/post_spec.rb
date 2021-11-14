22462ca7d2f843f5a6dc894160e193371d41db2b
Aaron Elizondo <acelizondo11@gmail.com>
2021-11-14 14:54:29 -0600

    Adding simple trsting for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:post) {FactoryBot.build(:post)}
+
+  describe "Associations" do
+    it{ should belong_to(:user) }
+    it{ should have_many(:comments) }
+    it{ should have_many(:likes) }
+    it{ should have_one_attached(:photo) }
+  end
 end
