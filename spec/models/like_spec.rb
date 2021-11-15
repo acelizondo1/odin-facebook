83d52a36af66214f013ebecca7218c667adf1d92
Aaron Elizondo <acelizondo11@gmail.com>
2021-11-14 19:22:31 -0600

    Adding testing for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:like) {FactoryBot.build(:like)}
+
+  describe "Associations" do
+    it { should belong_to :user }
+    it { should belong_to :post }
+    it { should have_many :notifications }
+  end
+
+  describe "Scopes" do
+    describe "#locate_like" do
+      it "should return the correct like record based off given post and user id" do
+        user = FactoryBot.create(:user)
+        post = user.posts.create!(body: "Test")
+        like = Like.create!(user: user, post: post)
+        expect(Like.locate_like(user, post).first).to eql(like)
+      end
+    end
+  end
 end
